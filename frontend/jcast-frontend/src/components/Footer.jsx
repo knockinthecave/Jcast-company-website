0370a3e4d93de523c77ab813fdc7389e0aff852e
성범 이 <crane2356@naver.com>
2024-02-06 18:26:48 +0900

    제품소개 영역 개요 완성. Header에서 제품소개 클릭 시 제품소개쪽으로 넘어가도록 설정 아직 못했음.

--- frontend/jcast-frontend/src/components/Footer.jsx
+++ frontend/jcast-frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer id="footer" role="contentinfo">
             <div className="footer__inner">
                 <div className="footer__right">
-                    © 2024 jcast networks<br />
+                    © 2024 (주)제이캐스트<br />
                 </div>
             </div>
         </footer>
