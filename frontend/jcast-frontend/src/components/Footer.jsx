a42e0d660bfe4dc2a6260d6bd7ff3d93d94d0e1c
성범 이 <crane2356@naver.com>
2024-02-07 19:10:53 +0900

    배포 테스트

--- frontend/jcast-frontend/src/components/Footer.jsx
+++ frontend/jcast-frontend/src/components/Footer.jsx
@@ -2,13 +2,16 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer id="footer" role="contentinfo">
-            <div className="footer__inner">
-                <div className="footer__right">
-                    © 2024 (주)제이캐스트<br />
+        <React.Fragment>
+            <footer id="footer" role="contentinfo">
+              <div className="footer__inner">
+                <div className="footer__content">
+                  <p className="footer__company-name"><strong>(주)제이캐스트 | 대표 : 어재홍</strong></p>
+                  <p className="footer__address">14056 경기도 안양시 동안구 벌말로 123 평촌스마트베이 A동 1105호</p>
                 </div>
-            </div>
-        </footer>
+              </div>
+            </footer>
+        </React.Fragment>
     );
 };
 
